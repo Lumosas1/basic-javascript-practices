abda1790c772750d0da67d64df996aceecf9bd78
Nesrin Eskiocak <nesrineskiocak68@gmail.com>
2025-07-14 04:52:41 +0300

    görev 1 tamamlandı

--- index.js
+++ index.js
@@ -23,8 +23,8 @@ function kareninAlani(kenaruzunlugu) {
 	4. Hesaplanan çemberin çevresi döndürülecektir.
 */
 
-function cemberinCevresi(/* kodlar buraya */) {
-  /* kodlar buraya */
+function cemberinCevresi(yaricap) {
+ return 2 * pi * yaricap;
 }
 
 /* (Oto test yok) Yukarıdaki cemberinCevresi fonksiyonunu yarıçap = 5 vererek aşağıda çalıştırıp, sonucu konsolda gözlemleyin (console.log)  */
